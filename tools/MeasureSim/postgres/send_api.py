0cf4d6785a363779206b9af6c920e8e1432b6cd1
ruimscarvalho98 <ruimsc98@gmail.com>
2020-11-17 11:30:13 +0000

    Added Clear fuction on sim and api

--- tools/MeasureSim/postgres/send_api.py
+++ tools/MeasureSim/postgres/send_api.py
@@ -4,6 +4,10 @@ import requests
 import time
 from pyowm import OWM
 
+# Default number of uploads
+DEFAULT_MAX_UPLOADS = 1e4
+DEFAULT_SAMPLE_PERIOD_SECS = 60
+
 # OpenWeatherMap
 OWM_API_URL = '33ad830a6029caa22792ea214464f035'
 
@@ -33,6 +37,12 @@ def retrieve_real_weather_data(mgr, location = 'Porto,Pt'):
     measurements['voltage'] = random.gauss(3.3, 0.1)
     return measurements
 
+def clear_db(url, debug =False):
+    res = requests.delete(url)
+    if debug:
+        print(res.content)
+    res_json = res.json()
+    print(f'Deleted {res_json["rowCount"]} rows')
 
 def publish_neasurements(url, measurements, debug = False):
     data = {
@@ -59,10 +69,13 @@ if __name__ == '__main__':
     parser.add_argument("--url", type=str, required=True, help='URL for upload measurements rest API endpoint')
     parser.add_argument("--real", action='store_true', help='Uses real data for OpenWeatherMap')
     parser.add_argument("--location", type=str, default='Porto, Pt', help='Specify location from openweathermap for temp, hum and windspeed ')
-    #parser.add_argument("--clear", action='store_true', help="Deletes DB before starting")
-    parser.add_argument("-n", type=int, default=100, help='Number of measurements')
-    parser.add_argument("-t", type=int, default=60, help='Period of measurements in seconds')
+    parser.add_argument("--clear", action='store_true', help="Deletes DB before starting")
+    parser.add_argument("-n", type=int, default=DEFAULT_MAX_UPLOADS, help='Number of measurements')
+    parser.add_argument("-t", type=int, default=DEFAULT_SAMPLE_PERIOD_SECS, help='Period of measurements in seconds')
     args = parser.parse_args()
+
+    if args.clear:
+        clear_db(args.url)
 
     if args.real:
         mgr = sign_in_openweatermap()
