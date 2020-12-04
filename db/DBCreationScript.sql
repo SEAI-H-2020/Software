74eb36b66e55ae7ff8c85390b03f502672f68e6d
JoseCSNeto <47570295+JoseCSNeto@users.noreply.github.com>
2020-12-04 16:52:54 +0000

    Add files via upload

---
+++ db/DBCreationScript.sql
@@ -0,0 +1,37 @@
+CREATE EXTENSION pgcrypto; --necessary for password encryption
+
+CREATE TABLE users
+(
+    username TEXT COLLATE pg_catalog."default" NOT NULL,
+    email TEXT COLLATE pg_catalog."default" NOT NULL,
+    password TEXT COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (username)
+) 
+
+CREATE TABLE measurements
+(
+    id serial NOT NULL,
+    tstamp timestamp without time zone DEFAULT now(),
+    temperature real,
+    humidity real,
+    wind real,
+    noise_level real,
+    voltage real,
+    CONSTRAINT measurements_pkey PRIMARY KEY (id)
+)
+
+
+CREATE TABLE configurations
+(
+    box integer NOT NULL,
+    sync_period integer NOT NULL,
+    sample_time integer NOT NULL,
+    shutdown_on_wakeup boolean,
+    username character varying(25) COLLATE pg_catalog."default",
+    CONSTRAINT configurations_pkey PRIMARY KEY (box),
+    CONSTRAINT username FOREIGN KEY (username)
+        REFERENCES users (username) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
