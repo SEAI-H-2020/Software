92c54989b02cb3526ff7c9741446643984003172
ruimscarvalho98 <ruimsc98@gmail.com>
2020-12-22 15:34:56 +0000

    change max measurements

--- sensorbox/include/esp_settings.h
+++ sensorbox/include/esp_settings.h
@@ -3,9 +3,7 @@
 //#define OTA_UPDATE
 //#define FIRMWARE_VERSION 0.2
 
-#define MAX_SYNC_PERIOD 30
-#define MAX_SAMPLE_PERIOD 600
-#define MAX_MEASUREMENTS MAX_SAMPLE_PERIOD/MAX_SYNC_PERIOD
+#define MAX_MEASUREMENTS 5
 
 //#define RESET_WIFI_SETTINGS   
 //#define INITIAL_DELAY         // initial delay of 5sec before printing out stuff
