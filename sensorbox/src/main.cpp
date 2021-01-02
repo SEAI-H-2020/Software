d838c00c9e7ae64db3dd3b27ea225e60bd5b32d2
ruimscarvalho98 <ruimsc98@gmail.com>
2021-01-02 19:17:29 +0000

    timestamping

--- sensorbox/src/main.cpp
+++ sensorbox/src/main.cpp
@@ -41,6 +41,45 @@ RTC_DATA_ATTR dssettings_t dss;
 RTC_DATA_ATTR uint8_t num_measurements = 0;
 RTC_DATA_ATTR uint8_t overflow_count = 0;
 
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 3600;
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+}
+
+
 // Based on https://github.com/pycom/esp-idf-2.0/blob/master/components/newlib/time.c
 uint64_t get_rtc_time_us(){
     SET_PERI_REG_MASK(RTC_CNTL_TIME_UPDATE_REG, RTC_CNTL_TIME_UPDATE_M);
@@ -143,7 +182,6 @@ void setup() {
 	  measurements[idx].windspeed = calculate_windspeed(wscounter,dss.wake_up_time*60);
     measurements[idx].windspeed += overflow_count * (2^13);
     clear_counter();
-
     Serial.println("######### WindSpeed MEASUREMENTS ###########");
 	  Serial.print("BIN: ");
 	  print_counter_state_bin(wscounter);
@@ -200,11 +238,25 @@ void setup() {
       Serial.println("Posted!!");
     }
     num_measurements = 0;
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   }
+  printLocalTime();
 
   float uptime = (float)esp_timer_get_time()*1e-6;
   Serial.print("Program duration:");
   Serial.println(uptime);
+
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+
+  //timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday
+  char str[50];
+  sprintf(str, "%02d-%02d-%02d %02d:%02d:%02d", timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  Serial.println(str); //2020-12-22 22:49:39'
+
 
   //-----------------------------------Sleep----------------------------------------
   gpio_hold_en(COUNTER_RESET_GPIO);
